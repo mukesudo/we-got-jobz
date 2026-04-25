e48c6b2dbf593b53b7dd81824849738829bf31ca
Mukhtar Saeed Ahmed <mukeee6677@gmail.com>
2026-04-25 19:43:13 +0300

    fix: Added redirect URI to point to backend

--- apps/backend/src/auth/config.ts
+++ apps/backend/src/auth/config.ts
@@ -76,10 +76,12 @@ export const auth = betterAuth({
     google: {
       clientId: process.env.GOOGLE_CLIENT_ID || '',
       clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
+      redirectURI: backendBaseURL
     },
     github: {
       clientId: process.env.GITHUB_CLIENT_ID || '',
       clientSecret: process.env.GITHUB_CLIENT_SECRET || '',
+      redirectURI: backendBaseURL
     },
   },
   trustedOrigins,
